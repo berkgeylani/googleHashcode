b8aebb86ae3cf523220ee5051b46b1cf28fd630b
francium <francium@francium-GL62-6QD>
2017-01-30 08:48:33 +0300

    Can find Iteratable Probility Pizza Slices. GC limit exceeded Without any parameter.(in big.in)

--- google-hashcode/src/main/java/org/kekosystem/pizza/ProbabiltyCreator.java
+++ google-hashcode/src/main/java/org/kekosystem/pizza/ProbabiltyCreator.java
@@ -13,7 +13,7 @@ import java.util.stream.Stream;
 public class ProbabiltyCreator {
 
 	private static boolean isFirst = true;
-//	private static char[][] pizzaTable;
+	// private static char[][] pizzaTable;
 	private static int R, C, L, H, index = 0;
 	private static List<List<CellPOJO>> pizzaTableAsList = new ArrayList<List<CellPOJO>>();
 
@@ -33,24 +33,24 @@ public class ProbabiltyCreator {
 				C = new Integer(attributes[1]).intValue();
 				L = new Integer(attributes[2]).intValue();
 				H = new Integer(attributes[3]).intValue();
-//				pizzaTable = new char[R][C];
+				// pizzaTable = new char[R][C];
 			} else {
 				pizzaTableAsList.add(new ArrayList<CellPOJO>());
 				char[] charsPerLine = line.toCharArray();
-				//index -1 = pizza nın satırı
-//				pizzaTable[index - 1] = charsPerLine;
+				// index -1 = pizza nın satırı
+				// pizzaTable[index - 1] = charsPerLine;
 				// Alttaki for döngüsü hangi malzemeden kaç tane var çıtkısını
 				// veriyor.
 				for (int i = 0; i < charsPerLine.length; i++) {
 					char gradient = charsPerLine[i];
-					pizzaTableAsList.get(index-1).add(new CellPOJO(2*(index-1),2*i,gradient));
-					if (gradient=='T') {
+					pizzaTableAsList.get(index - 1).add(new CellPOJO(2 * (index - 1), 2 * i, gradient));
+					if (gradient == 'T') {
 						if (gradientCount.containsKey('T')) {
 							gradientCount.put('T', gradientCount.get('T') + 1);
 						} else {
 							gradientCount.put('T', 1);
 						}
-					} else if (gradient=='M') {
+					} else if (gradient == 'M') {
 						if (gradientCount.containsKey('M')) {
 							gradientCount.put('M', gradientCount.get('M') + 1);
 						} else {
@@ -61,101 +61,68 @@ public class ProbabiltyCreator {
 			}
 			index++;
 		});
-//		 prerequirities finisihed.
+		// prerequirities finisihed.
 		for (List<CellPOJO> row : pizzaTableAsList) {
 			for (CellPOJO cell : row) {
-				System.out.print(cell.toString()+"\t");
+				System.out.print(cell.toString() + "\t");
 			}
 			System.out.println();
 		}
-		
-		
-		// Lets get probabilities
-		/*
-		 * Öncelikle birnici ışın için bütün ihtimalleri aldığımız algoritmayı yazacağız.
-		 * 3*5(3 satır 5 sütun olan bir matris)elemanları
-		 * 0,0	0,1 0,2
-		 * 1,0	1,1	1,2
-		 * string indexleri bu şekilde biz ise bunları kordinat düzleminde(x,y) olarak tutacağız.Tuttuk şimdi ise;
-		 * bunları bölebileceğimiz xden1 xden 3 x den 5 yden1 yden 3 gibi çizgilerimi bulmamız gerekiyor.bulduk şimdi ise;
-		 * bunları buldukya
-		 * 
-		 * */
-		System.out.println("***************-------------***************");
-		for (int i = 0; i < R-1; i++) {
-			int dividerLineAtX = (i)*2+1;
-			System.out.println("x bölücüsü :\t"+ dividerLineAtX);
-			//burada bölünen çizgi var zaten arraylistin o indexten küçük ve büyük bölgelerini alsam yeter.
-			//bunlarda 2 ye ayrılacak nodelar olacak master nodden ayrılan
-			//algroritma ise her satır başındaki cell çekilip x değerine bakılacak 
-			//eğer küçükse ilk slice a dahil
-			//eğer büyükse ikinci slice a dahil olacak.
-			//bunun yerine küççükmü büyükmü diye bakmak  yerine elimizdeki i nin
-			// değeri mesela 0 ise sadece il satır dahil alıncak yani x.substring(0,1)
-			//2 olsaydı 1 ve ikinci satırlar alınacak kalan satırlar 2. slice a dahil
-			//yani genellerse i kadar satırı alıp birinci slice a alıcaz
-			String lineId = System.currentTimeMillis()+"-x"+i;
-			TableNode topSlice = new TableNode(lineId, new ArrayList<>(pizzaTableAsList.subList(0, i+1)));
-			TableNode bottomSlice = new TableNode(lineId, new ArrayList<>(pizzaTableAsList.subList(i+1, pizzaTableAsList.size())));
-			topSlice.printTable();
-			System.out.println();
-			System.out.println();
-			bottomSlice.printTable();
-			System.out.println();
-			System.out.println("***************-------------***************");
-			System.out.println();
-		}
+		TableNode mainNode = new TableNode("000", pizzaTableAsList);
+		printSLices(mainNode);
 		System.out.println();
 		System.out.println();
-		System.out.println("***************-------------***************");
-		for (int i = 0; i < C-1; i++) {
-			System.out.println("y bölücüsü :\t "+ ((i)*2+1));
-			List<List<CellPOJO>> leftSliceAsAL = new ArrayList<List<CellPOJO>>();
-			List<List<CellPOJO>> rightSliceAsAL= new ArrayList<List<CellPOJO>>();
-			for (int rowIndex = 0; rowIndex < R; rowIndex++) {
-				leftSliceAsAL.add(new ArrayList<CellPOJO>(pizzaTableAsList.get(rowIndex).subList(0, i+1)));
-				rightSliceAsAL.add(new ArrayList<CellPOJO>(pizzaTableAsList.get(rowIndex).subList(i+1, pizzaTableAsList.get(rowIndex).size())));
-			}
-			TableNode leftSlice = new TableNode(System.currentTimeMillis()+"-y"+i, leftSliceAsAL);
-			TableNode rightSlice = new TableNode(System.currentTimeMillis()+"-y"+i, rightSliceAsAL);
-			leftSlice.printTable();
+		
+		mainNode.getSubSliceTable().forEach((k,v)->{
+			System.out.println("----------------------111-------------");
+			v[0].printTable();
+			printSLices(v[0]);
+			System.out.println("----------------------222-------------");
+			v[1].printTable();
+			printSLices(v[1]);
 			System.out.println();
 			System.out.println();
-			rightSlice.printTable();
 			System.out.println();
-			System.out.println("***************-------------***************");
 			System.out.println();
-		}
+		});
 		
 		
-		
-//		for (int i = 1; i <= ((gradientCount.get('T') < gradientCount.get('M')) ? gradientCount.get('T')
-//				: gradientCount.get('M'))/* Az olan malzemenin sayısını getir */; i++) {
-//			int lineCount=i-1;
-//			for (int j = 0; j < combination((R-1)*(C-1), lineCount); j++) {
-//				
-//			}
-//		}
-	}
-	public static int combination(int n, int k)
-	{
-	    return permutation(n) / (permutation(k) * permutation(n - k));
 	}
 
-	public static int permutation(int i)
-	{
-	    if (i == 1)
-	    {
-	        return 1;
-	    }
-	    return i * permutation(i - 1);
+	private static void printSLices(TableNode mainNode) {
+		mainNode.getSubSliceTable().forEach((k,v)->{
+			System.out.println("\n____________________"+k+"____________________\n");
+			for (int i = 0; i < v.length; i++) {
+				System.out.println("**********************************************");
+				v[i].printTable();
+			}
+		});
 	}
-//	private static void printPizzaTable() {
-//		for (int i = 0; i < pizzaTable.length; i++) {
-//			for (int j = 0; j < pizzaTable[i].length; j++) {
-//				System.out.print(pizzaTable[i][j]);
-//			}
-//			System.out.println();
-//		}
-//	}
+
+	// for (int i = 1; i <= ((gradientCount.get('T') < gradientCount.get('M')) ?
+	// gradientCount.get('T')
+	// : gradientCount.get('M'))/* Az olan malzemenin sayısını getir */; i++) {
+	// int lineCount=i-1;
+	// for (int j = 0; j < combination((R-1)*(C-1), lineCount); j++) {
+	//
+	// }
+	// }
+	public static int combination(int n, int k) {
+		return permutation(n) / (permutation(k) * permutation(n - k));
+	}
+
+	public static int permutation(int i) {
+		if (i == 1) {
+			return 1;
+		}
+		return i * permutation(i - 1);
+	}
+	// private static void printPizzaTable() {
+	// for (int i = 0; i < pizzaTable.length; i++) {
+	// for (int j = 0; j < pizzaTable[i].length; j++) {
+	// System.out.print(pizzaTable[i][j]);
+	// }
+	// System.out.println();
+	// }
+	// }
 }
--- google-hashcode/src/main/java/org/kekosystem/pizza/TableNode.java
+++ google-hashcode/src/main/java/org/kekosystem/pizza/TableNode.java
@@ -1,49 +1,144 @@
 package org.kekosystem.pizza;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 
 public class TableNode {
 	private String lineId;
-	private List<List<CellPOJO>> pizzaSlice= new ArrayList<List<CellPOJO>>();// yourList will be 2D arraylist.
-	private static int R, C;
+	private List<List<CellPOJO>> pizzaSlice = new ArrayList<List<CellPOJO>>();// yourList
+																				// will
+																				// be
+																				// 2D
+																				// arraylist.
+	private int R=0, C=0;
+	private Hashtable<String, TableNode[]> sliceTable = new Hashtable<>();
 
 	public String getLineId() {
 		return lineId;
 	}
+
 	public void setLineId(String lineId) {
 		this.lineId = lineId;
 	}
+
 	public List<List<CellPOJO>> getPizzaSlice() {
 		return pizzaSlice;
 	}
+
 	public void setPizzaSlice(List<List<CellPOJO>> pizzaSlice) {
 		this.pizzaSlice = pizzaSlice;
 	}
-	
-	public void printSliceDividerLines(){
-		for (int i = 0; i < R-1; i++) {
-			System.out.println("x bölücüsü :\t"+ ((i)*2+1));
+
+	public void printSliceDividerLines() {
+		for (int i = 0; i < R - 1; i++) {
+			System.out.println("x bölücüsü :\t" + ((i) * 2 + 1));
 		}
-		for (int i = 0; i < C-1; i++) {
-			System.out.println("y bölücüsü :\t "+ ((i)*2+1));
+		for (int i = 0; i < C - 1; i++) {
+			System.out.println("y bölücüsü :\t " + ((i) * 2 + 1));
 		}
 	}
-	
+
+	public Hashtable<String, TableNode[]> getSubSliceTable() {
+		return sliceTable;
+	}
+
+	public void setSliceTable(Hashtable<String, TableNode[]> sliceTable) {
+		this.sliceTable = sliceTable;
+	}
+
 	public TableNode(String lineId, List<List<CellPOJO>> pizzaSlice) {
-		super();
 		this.lineId = lineId;
 		this.pizzaSlice = pizzaSlice;
+		this.R=pizzaSlice.size();
+		if(!pizzaSlice.isEmpty()){
+			C=pizzaSlice.get(0).size();
+		}
+		findProbabilities();
+		//findProbabilities will find possible partition of pizza.
 	}
+
 	private CellPOJO getTopRightCornerCell() {
 		return getPizzaSlice().get(0).get(0);
 	}
+
 	public void printTable() {
 		for (List<CellPOJO> row : getPizzaSlice()) {
 			for (CellPOJO cell : row) {
-				System.out.print(cell.toString()+"\t");
+				System.out.print(cell.toString() + "\t");
 			}
 			System.out.println();
 		}
+	}
+
+	public Hashtable<String, TableNode[]> findProbabilities() {
+		TableNode[] slices;
+		for (int i = 0; i < R - 1; i++) {
+			slices = new TableNode[2];
+			int dividerLineAtX = (i) * 2 + 1+getTopRightCornerCell().getX();
+//			System.out.println("x bölücüsü :\t" + dividerLineAtX);
+			// burada bölünen çizgi var zaten arraylistin o indexten küçük ve
+			// büyük bölgelerini alsam yeter.
+			// bunlarda 2 ye ayrılacak nodelar olacak master nodden ayrılan
+			// algroritma ise her satır başındaki cell çekilip x değerine
+			// bakılacak
+			// eğer küçükse ilk slice a dahil
+			// eğer büyükse ikinci slice a dahil olacak.
+			// bunun yerine küççükmü büyükmü diye bakmak yerine elimizdeki i nin
+			// değeri mesela 0 ise sadece il satır dahil alıncak yani
+			// x.substring(0,1)
+			// 2 olsaydı 1 ve ikinci satırlar alınacak kalan satırlar 2. slice a
+			// dahil
+			// yani genellerse i kadar satırı alıp birinci slice a alıcaz
+			String lineIdForX = System.currentTimeMillis() + "-x" + dividerLineAtX;
+			TableNode topSlice = new TableNode(lineIdForX, new ArrayList<>(getPizzaSlice().subList(0, i + 1)));
+			TableNode bottomSlice = new TableNode(lineIdForX,
+					new ArrayList<>(getPizzaSlice().subList(i + 1, getPizzaSlice().size())));
+			slices[0] = topSlice;
+			slices[1] = bottomSlice;
+			sliceTable.put(lineIdForX, slices);
+		}
+		for (int i = 0; i < C - 1; i++) {
+			int dividerLineAtY = (i) * 2 + 1+getTopRightCornerCell().getY();
+			slices = new TableNode[2];
+//			System.out.println("y bölücüsü :\t " + ((i) * 2 + 1));
+			List<List<CellPOJO>> leftSliceAsAL = new ArrayList<List<CellPOJO>>();
+			List<List<CellPOJO>> rightSliceAsAL = new ArrayList<List<CellPOJO>>();
+			for (int rowIndex = 0; rowIndex < R; rowIndex++) {
+				leftSliceAsAL.add(new ArrayList<CellPOJO>(getPizzaSlice().get(rowIndex).subList(0, i + 1)));
+				rightSliceAsAL.add(new ArrayList<CellPOJO>(
+						getPizzaSlice().get(rowIndex).subList(i + 1, getPizzaSlice().get(rowIndex).size())));
+			}
+			String lineIdForY = System.currentTimeMillis() + "-y" + dividerLineAtY;
+			TableNode leftSlice = new TableNode(lineIdForY, leftSliceAsAL);
+			TableNode rightSlice = new TableNode(lineIdForY, rightSliceAsAL);
+			slices[0] = leftSlice;
+			slices[1] = rightSlice;
+			sliceTable.put(lineIdForY, slices);
+		}
+		return sliceTable;
+	}
+
+	public Hashtable<Character,Integer> getGredientStatistics() {
+		Hashtable<Character, Integer> gradientCount = new Hashtable<>();
+		for (int i = 0; i < getPizzaSlice().size(); i++) {
+			for (int j = 0; j < getPizzaSlice().get(i).size(); j++) {
+				char gradient = getPizzaSlice().get(i).get(j).getGradient();
+				if (gradient == 'T') {
+					if (gradientCount.containsKey('T')) {
+						gradientCount.put('T', gradientCount.get('T') + 1);
+					} else {
+						gradientCount.put('T', 1);
+					}
+				} else if (gradient == 'M') {
+					if (gradientCount.containsKey('M')) {
+						gradientCount.put('M', gradientCount.get('M') + 1);
+					} else {
+						gradientCount.put('M', 1);
+					}
+				}
+			}
+		}
+	return gradientCount;
 	}
 }
